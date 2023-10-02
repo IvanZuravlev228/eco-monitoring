6ad3dac7c276f482cfed5faf70560f5b265673e4
IvanZuravlev228 <courses.zhuravlev.ivan@gmail.com>
2023-10-02 16:36:10 +0300

    update add function

--- angular/src/app/components/pollution/pollution.component.ts
+++ angular/src/app/components/pollution/pollution.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PollutionService} from "../../services/pollution.service";
 import {Pollution} from "../../model/Pollution";
 import {PollutionForShow} from "../../model/PollutionForShow";
+import {CompanyServiceService} from "../../services/company-service.service";
+import {Company} from "../../model/Company";
+import {Pollutant} from "../../model/Pollutant";
+import {PollutantService} from "../../services/pollutant.service";
 
 @Component({
   selector: 'app-pollution',
   templateUrl: './pollution.component.html',
   styleUrls: ['./pollution.component.css']
 })
-export class PollutionComponent {
+export class PollutionComponent implements OnInit{
   pollutions: Pollution[] = [];
   pollutionsForShow: PollutionForShow[] = [];
   pollutionForSave: PollutionForShow = new PollutionForShow();
+  companies: Company[] = [];
+  pollutants: Pollutant[] = [];
 
-  constructor(private pollutionService: PollutionService) {
+  constructor(private pollutionService: PollutionService,
+              private companiesService: CompanyServiceService,
+              private pollutantService: PollutantService) {
   }
 
-  getAllPollution() {
+  getAllPollutionForShow() {
     this.pollutionService.getAllPollutionForShow().subscribe({
       next: (pollutions) => {
         this.pollutionsForShow = pollutions;
@@ -36,5 +44,32 @@ export class PollutionComponent {
         console.log(error);
       }
     })
+  }
+
+  getAllCompanies() {
+    this.companiesService.getAllCompany().subscribe({
+      next: (companies) => {
+        this.companies = companies;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  getAllPollutants() {
+    this.pollutantService.getAllPollutant().subscribe({
+      next: (poll) => {
+        this.pollutants = poll;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.getAllCompanies();
+    this.getAllPollutants();
   }
 }
